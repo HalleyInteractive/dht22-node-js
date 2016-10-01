6405080c0e10ce49afdaa4cf6c63dff63b0b918b
Niels Overwijn <niels.overwijn@gmail.com>
2016-10-01 19:33:26 +0100

    Send reading to host

--- index.js
+++ index.js
@@ -4,9 +4,20 @@
   const dht22 = require('./dht22.js');
   const request = require('request');
   const READING_INTERVAL = 1000;
+  const HOST = 'http://localhost:3000';
+  const NODE_ID = 'rpi_hub';
 
   setInterval(() => {
-    console.log(dht22.read());
+    let reading = dht22.read();
+    let body = {
+      temperature: reading.temperature,
+      humidity: reading.humidity
+    };
+    request.put(`${HOST}/nodes/${NODE_ID}/reading`)
+    .send(body)
+    .end(() => {
+      console.log('Send reading to host');
+    });
   }, READING_INTERVAL);
 
 }());
